d6ba6278adf858e799f0d873d66cd96700591198
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-06-13 20:52:59 -0700

    Add Lottery component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Ball from './Ball'
+import Lottery from './Lottery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lottery />
+      <Lottery title="Mini Daily" maxBalls={4} maxNum={10} /> 
     </div>
   );
 }
